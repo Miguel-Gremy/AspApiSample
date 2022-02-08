1db21e3eeea99e5636153842e4031ed908abeb13
GREMY Miguel <miguel.gremy72@gmail.com>
2022-02-08 15:06:44 +0100

    - Add MultiSelectDropdown support throught bootstrap and JQuery support         - Add a select with asp-for and asp-items (as MultiSelectList), add 'multiple' tag and 'multiselect multiselect-filter form-control' class to work

--- AspApiSample.Web/wwwroot/js/site.js
+++ AspApiSample.Web/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Enable MultiSelect from bootstrap
+$(function () {
+    $('.multiselect').multiselect({
+        includeSelectAllOption: true,
+        enableFiltering: true
+    });
+});
